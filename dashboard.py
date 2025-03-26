a2061c7c397d91e4defc01cd49680ae8b6465b1d
JERRIYHAN <71117561+JERRIYHAN@users.noreply.github.com>
2025-03-26 13:05:36 +0800

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -8,8 +8,9 @@ pages = {
         st.Page("pages/main_page.py", title="@JERRIYHAN")
     ],
     "个人简介": [
-        st.Page("pages/Pprofile.py", title="我是谁？")
+        st.Page("pages/Pprofile.py", title="我是谁？"),
     ]
 }
+
 pg = st.navigation(pages)
 pg.run()
